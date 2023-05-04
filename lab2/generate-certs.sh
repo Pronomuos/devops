a790e60d4754f904347c2eb204423f880b762858
Pronomuos <WizardPronomuos@yandex.ru>
2023-05-04 16:51:20 +0300

    Update: labs 1-5

---
+++ lab2/generate-certs.sh
@@ -0,0 +1,6 @@
+#bin/bin/sh
+
+while true; do
+  openssl req -x509 -newkey rsa:4096 -nodes -keyout /certs/key.pem -out /certs/cert.pem -days 1 -subj "/C=US/ST=Denial/L=Springfield/D=Dis/CN=localhost"
+  sleep 86400
+done
